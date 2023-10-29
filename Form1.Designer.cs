12d65597965bd7e500c3b752f481d9dee3797a3c
barclaybarnes <147193719+barclaybarnes@users.noreply.github.com>
2023-10-29 19:59:52 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Project_GUI_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CreateWorkout = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ViewWorkouts = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CreateWorkout
+            // 
+            this.CreateWorkout.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateWorkout.Location = new System.Drawing.Point(418, 100);
+            this.CreateWorkout.Name = "CreateWorkout";
+            this.CreateWorkout.Size = new System.Drawing.Size(150, 50);
+            this.CreateWorkout.TabIndex = 0;
+            this.CreateWorkout.Text = "Create Workout";
+            this.CreateWorkout.UseVisualStyleBackColor = true;
+            this.CreateWorkout.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(350, 17);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(285, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "WorkoutBot";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ViewWorkouts
+            // 
+            this.ViewWorkouts.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ViewWorkouts.Location = new System.Drawing.Point(418, 156);
+            this.ViewWorkouts.Name = "ViewWorkouts";
+            this.ViewWorkouts.Size = new System.Drawing.Size(150, 50);
+            this.ViewWorkouts.TabIndex = 2;
+            this.ViewWorkouts.Text = "View Workouts";
+            this.ViewWorkouts.UseVisualStyleBackColor = true;
+            this.ViewWorkouts.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(418, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Frequently Asked Questions";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-7, -23);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(992, 592);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.CreateWorkout);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.ViewWorkouts);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(984, 566);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(274, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(984, 543);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(527, 255);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(850, 504);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 25);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Clear Workout File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(527, 297);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 50);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Create Set";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 504);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 25);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Back";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(527, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(355, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Workout Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(527, 205);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(415, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Reps";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(415, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Sets";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(350, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(375, 55);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Create Workout";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "WorkoutBot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateWorkout;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ViewWorkouts;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Project_GUI_1._0
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName; //Add Mac Operating System Compatible version later
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Create Workout Button
+        {
+            DialogResult dr = MessageBox.Show("Do you want to create a new Workout?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes) 
+            {
+                tabControl1.SelectedIndex = 1;
+            }     
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) // View Workouts Button Calls General Path + Visual Studio Files
+        {
+            Process.Start(path+"\\bin\\debug\\WorkoutList.txt");
+        }
+
+        private void button1_Click_2(object sender, EventArgs e) //Code Works outside of this yet does not work when allocating the console here? I need a fix for this to move forward with the chatbot function of the app
+        {
+            
+                AllocConsole();
+
+                Console.WriteLine("Welcome to the Workout Generator App! \n");
+
+                Console.WriteLine("Below are a list of our Frequently Asked Questions:");
+
+                int MenuInt = 0;
+
+                do
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine
+                       ("\n" +
+                        "1) What are the benefits of being Active?\n" +
+                        "2) Can I tailor my Workout to my preferences or fitness goals?\n" +
+                        "3) Is there a variety of workout options available, or does the app offer a specific type of workout?\n" +
+                        "4) Are there video demonstrations or detailed instructions for each exercise in the generated workout?\n" +
+                        "5) Can I sync the app with my smart devices or wearables to monitor my physical activity throughout the day?\n" +
+                        "6) Are there instructional videos or guides to ensure I'm performing exercises with proper form and technique?\n" +
+                        "7) Link to a Short Video explaining use of the Application\n" +
+                        "8) Stop");
+
+
+                    MenuInt = 0;
+                    MenuInt = Convert.ToInt32(Console.ReadLine());
+
+
+                    switch (MenuInt)
+                    {
+                        case 1:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\n Benefits include improved thinking or cognition for children 6 to 13 years of age and reduced short-term feelings of anxiety for adults.\n Regular physical activity can help keep your thinking, learning, and judgment skills sharp as you age. It can also reduce your risk of depression and anxiety and help you sleep better.");
+                            break;
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\n You can tailor your workout to your preferences or your fitness goals.\n There is a wide variety of workout options available depending on what your goals are ranging from simply walking to weight lifting");
+                            break;
+                        case 3:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\n There is a wide variety of workout options available depending on your personal preferences and what you want to acheive. \n This can range from: \n Weightlifting \n Walking or Running \n Sports \n Yoga \n etc...");
+                            break;
+                        case 4:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\n Yes, there is either a video demonstration of the workout or a detailed description of the workout");
+                            break;
+                        case 5:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\n No, the functionality that is currently available does not allow the app to be synced with smart devices to track activity.");
+                            break;
+                        case 6:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\n Yes, there are in some cases videos or detailed instruction there to ensure the proper form and technique");
+                            break;
+                        case 7:
+                            {
+
+                                string url = "http://google.com";
+
+
+                                try
+                                {
+                                    Process.Start(url);
+                                }
+                                catch
+                                {
+                                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                                    {
+                                        url = url.Replace("&", "^&");
+                                        Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                                    }
+                                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                                    {
+                                        Process.Start("xdg-open", url);
+                                    }
+                                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                                    {
+                                        Process.Start("open", url);
+                                    }
+                                    else
+                                    {
+                                        throw;
+                                    }
+                                }
+                            }
+                            break;
+                        case 8:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\n Thank you for using our Frequently Asked Questions feature.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                    }
+
+                } while (MenuInt != 8);
+
+
+
+
+
+
+
+            
+        }
+        
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Swapping Pages to Create Workout Page
+        {
+            tabControl1.SelectedIndex = 0;
+        }
+       
+        private void button3_Click(object sender, EventArgs e) //Creating New Set
+        {
+            DialogResult dr = MessageBox.Show("Do you want to create a new Set?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            
+            if (dr == DialogResult.Yes)
+            {
+                if (!File.Exists("WorkoutList.txt")) // If file does not exists
+                {
+                    File.Create("WorkoutList.txt").Close(); // Create file
+                    using (StreamWriter sw = File.AppendText("WorkoutList.txt"))
+                    {
+                        sw.WriteLine(textBox3.Text); // Write text to .txt file
+                        sw.WriteLine(textBox1.Text + " Sets");
+                        sw.WriteLine("Of " + textBox4.Text + " Reps\n");
+                    }
+                }
+                else // If file already exists
+                {
+                    // File.WriteAllText("FILENAME.txt", String.Empty); // Clear file
+                    using (StreamWriter sw = File.AppendText("WorkoutList.txt"))
+                    {
+                        sw.WriteLine(textBox3.Text); // Write text to .txt file
+                        sw.WriteLine(textBox1.Text + " Sets");
+                        sw.WriteLine("Of " + textBox4.Text + " Reps\n");
+                        
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) //Delete Workouts Function (Deletes all Text within Document)
+        {
+            DialogResult dr = MessageBox.Show("This will Delete All Previous Workouts. Are you sure you want to proceed?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                File.WriteAllText(path + "\\bin\\debug\\WorkoutList.txt", String.Empty);
+            }
+        }
+    }
+}
+
